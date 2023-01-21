70469261b59759c2ecaf03dae321746965aecb3c
sachinc <sachin@milenzer.com>
2023-01-21 23:11:13 +0530

    fix linting issues

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
+
 import styles from '../styles';
-import { navVariants } from "../utils/motion";
- 
+import { navVariants } from '../utils/motion';
+
 const Navbar = () => (
   <motion.nav
     variants={navVariants}
@@ -12,23 +13,21 @@ const Navbar = () => (
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
-
-    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <img 
+    <div
+      className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+    >
+      <img
         src="/search.svg"
         alt="search"
         className="w-[24px] h-[24px] object-contain"
       />
-      <h2
-        className="font-extrabold text-[24px] leading-[30px] text-white"
-      >
+      <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
         METAVERSUS
       </h2>
-      <img 
+      <img
         src="/menu.svg"
         alt="menu"
         className="w-[24px] h-[24px] object-contain"
-
       />
     </div>
   </motion.nav>
